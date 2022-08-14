d2e4589a57a251c8cbd084796ef277b7b1f4c109
Nij Mehar <k1ra@Nijs-MacBook-Air.local>
2022-08-14 19:56:19 +0530

    line sep

--- 01/part_d.java
+++ 01/part_d.java
@@ -5,53 +5,53 @@ import java.io.FileWriter;
 import java.util.Scanner;
 class part_d
 {
+    static void whileLoop(StringBuffer buffer, String path) throws FileNotFoundException{
+        try {
+            Scanner sc = new Scanner(new File(path));
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if(line.contains(".h")) {
+                    addStringFromExternal(buffer,line);
+                    continue;
+                }
+                if(line.isEmpty()) continue;
+                line = line.trim();
+                if(line.contains("/*")){
+                    try{
+                        while(!line.contains("*/")){    
+                            line = sc.nextLine();}
+                        continue;
+                    } catch(Exception e){
+                        System.out.println(e);
+                    }      
+                }
+                if(line.contains("//")) continue;
+                buffer.append(line+"\n");     
+            }
+        sc.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+}
     static void addStringFromExternal(StringBuffer buffer ,String line) throws FileNotFoundException{
-        String path = line.substring(10,line.length()-1);
-        Scanner sc = new Scanner(new File(path));
-        while(sc.hasNextLine()) {
-            line = sc.nextLine();
-            if(line.contains(".h")) {
-                addStringFromExternal(buffer,line);
-                continue;
-            }
-            line = line.trim();
-            if(line.contains("/*")){
-                while(!line.contains("*/")) sc.nextLine();
-                sc.nextLine();
-            }
-            if(line.contains("//")) continue;
-            buffer.append(line+"\n");
+        try{
+            String path = line.substring(10,line.length()-1);
+            whileLoop(buffer,path);
+        }
+        catch(Exception e){
+            System.out.println(e);
         }
     }
     public static void main(String[] args) throws Exception
     {   
-        Scanner sc = new Scanner(new File("01d_test1.c"));
-        // Scanner scMyH = new Scanner(new File("01_test_cases/my.h"));
+       try{ String path = "01d_test1.c";
         StringBuffer buffer = new StringBuffer();
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            if(line.contains(".h")) {
-                addStringFromExternal(buffer,line);
-                continue;
-            }
-            if(line.isEmpty()) continue;
-            line = line.trim();
-            if(line.contains("/*")){
-                try{
-                    while(!line.contains("*/")){    
-                        line = sc.nextLine();}
-                    continue;
-                } catch(Exception e){
-                    System.out.println(e);
-                }      
-            }
-            if(line.contains("//")) continue;
-            buffer.append(line+"\n");
-        }
+        whileLoop(buffer,path);
         String fileContents = buffer.toString();
-        sc.close();
         BufferedWriter writer = new BufferedWriter(new FileWriter("01d_test1_output.c"));
         writer.write(fileContents);
-        writer.flush();
+        writer.flush();}catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
