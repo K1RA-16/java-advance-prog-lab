0715991f09f0b790574b44d70fae2ac675cdde90
Nij Mehar <k1ra@Nijs-MacBook-Air.local>
2022-08-14 19:35:12 +0530

    corrected 1_d

--- 01/example.c
+++ 01/example.c
@@ -1,31 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-void iterFib(int n){
-long long first = 0, second = 1, result, i;
-for (i = 0; i < n; i++)
-{
-if (i <= 1)
-result = i;
-else
-{
-result = first + second;
-first = second;
-second = result;
-}
-printf(" %d", result);
-}
-}
-long long recFib(long long n){
-if(n == 0){
-return 0;
-} else if(n == 1) {
-return 1;
-} else {
-return (recFib(n-1) + recFib(n-2));
-}
-}
-void main(){
-long long n;
-scanf("%d", &n);
-printf("%d",recFib(n));
+#define R1 200
+int f(int);
+#define R 100
+int     main () {
+int x,   A[R];
+z = f(x-R);
+printf("z  	=  	%d,R1 = %d",z,   R1);
+return     0;
 }
--- 01/part_d.java
+++ 01/part_d.java
@@ -1,23 +1,54 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.Scanner;
 class part_d
 {
+    static void addStringFromExternal(StringBuffer buffer ,String line) throws FileNotFoundException{
+        String path = line.substring(10,line.length()-1);
+        Scanner sc = new Scanner(new File(path));
+        while(sc.hasNextLine()) {
+            line = sc.nextLine();
+            if(line.contains(".h")) {
+                addStringFromExternal(buffer,line);
+                continue;
+            }
+            line = line.trim();
+            if(line.contains("/*")){
+                while(!line.contains("*/")) sc.nextLine();
+                sc.nextLine();
+            }
+            if(line.contains("//")) continue;
+            buffer.append(line+System.lineSeparator());
+        }
+    }
     public static void main(String[] args) throws Exception
     {   
-        String filePath = "example.c";
-        Scanner sc = new Scanner(new File(filePath));
+        Scanner sc = new Scanner(new File("01d_test1.c"));
+        // Scanner scMyH = new Scanner(new File("01_test_cases/my.h"));
         StringBuffer buffer = new StringBuffer();
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
+            if(line.contains(".h")) {
+                addStringFromExternal(buffer,line);
+                continue;
+            }
             if(line.isEmpty()) continue;
             line = line.trim();
+            if(line.contains("/*")){
+                try{
+                    while(!line.contains("*/")){    
+                        line = sc.nextLine();}
+                    continue;
+                } catch(Exception e){
+                    System.out.println(e);
+                }      
+            }
             if(line.contains("//")) continue;
             buffer.append(line+System.lineSeparator());
         }
         String fileContents = buffer.toString();
-        System.out.println("Contents of the file: "+fileContents);
         sc.close();
         BufferedWriter writer = new BufferedWriter(new FileWriter("example.c"));
         writer.write(fileContents);
